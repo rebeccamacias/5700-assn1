6f83c50de1140b8807adaf28223c15e5659fd69c
beccazitting <rebeccazitting20@gmail.com>
2022-05-15 21:59:17 -0500

    write ellipse class and tests. most likely coming back to add to tests

--- src/main/kotlin/Ellipse.kt
+++ src/main/kotlin/Ellipse.kt
@@ -1,2 +1,39 @@
-class Ellipse {
+class Ellipse() : Shape() {
+    var centerPoint = MyPoint()
+        private set
+    var radius1: Double = 1.0
+        private set
+    var radius2: Double = 1.0
+        private set
+
+    fun setCenterPoint(point: MyPoint) {
+        centerPoint = point
+    }
+
+    fun setRadius1(radius: Double) {
+        if (radius <= 0){
+            println("Radius must be greater than 0. Setting radius to 1")
+            this.radius1 = 1.0
+        } else {
+            this.radius1 = radius
+        }
+    }
+
+    fun setRadius2(radius: Double) {
+        if (radius <= 0){
+            println("Radius must be greater than 0. Setting radius to 1")
+            this.radius2 = 1.0
+        } else {
+            this.radius2 = radius
+        }
+    }
+
+    override fun getArea(): Double {
+        return Math.PI * radius1 * radius2
+    }
+
+    override fun moveShape(xDelta: Double, yDelta: Double) {
+        centerPoint.setX(centerPoint.x + xDelta)
+        centerPoint.setY(centerPoint.y + yDelta)
+    }
 }
